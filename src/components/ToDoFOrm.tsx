f17ae9b0a0e37ec6500b1d47d9523e4d5a4cb9be
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-07-28 07:31:36 +0600

    displaying to user before backend and use previous daa if error on update

--- src/components/ToDoFOrm.tsx
+++ src/components/ToDoFOrm.tsx
@@ -2,32 +2,51 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
+
+interface addTodoContex {
+  previousTodos: Todo[];
+}
 const ToDoFOrm = () => {
   const queryClient = useQueryClient();
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, addTodoContex>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+      if (ref.current) ref.current.value = "";
+
+      return { previousTodos };
+    },
+
     onSuccess: (savedTodo, newTodo) => {
-      console.log(newTodo, savedTodo);
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+      //   console.log(newTodo, savedTodo);
       //APProach: Invalidating the cache
       // queryClient.invalidateQueries({
       //     queryKey: 'todos'
       // }),
 
       //Approach: Updating the data in the cache
+    },
 
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
-      if (ref.current) ref.current.value = "";
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>([todos], context.previousTodos);
     },
   });
 
-  const ref = useRef<HTMLInputElement>();
+  const ref = useRef<HTMLInputElement>(null);
   return (
     <>
       {addTodo.error && (
