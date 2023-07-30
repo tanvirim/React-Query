47147e712beefc50dec494d4d4ce6bff865689f3
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-07-30 23:32:55 +0600

    Added React DevTools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from 'simple-zustand-devtools'
 
 interface CounterStore {
     counter : number ;
@@ -12,5 +13,8 @@ const useCounterStore = create<CounterStore>(set=>({
     increment: ()=>set(store=> ({counter: store.counter+1})),
     reset : ()=> set(()=>({counter:0}))
 }) )
+
+if(process.env.NODE_ENV === 'development')
+    mountStoreDevtool('Counter Store', useCounterStore)
 
 export default useCounterStore
