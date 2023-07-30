3ff1109d5f878f3a4400fafcf33d8a97c8641916
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-07-30 17:42:47 +0600

    using zustand for auth state

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,4 +1,4 @@
-import LoginStatus from './LoginStatus';
+import LoginStatus from './auth/LoginStatus';
 
 import useCounterStore from './counter/store';
 
---
+++ src/state-management/auth/LoginStatus.tsx
@@ -0,0 +1,27 @@
+
+import useAuthStore from "./store";
+
+const LoginStatus = () => {
+   const {user , login , logout }=useAuthStore()
+
+  if (user)
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => logout()} href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+  return (
+    <div>
+      <a onClick={() => login('tanvir')} href="#">
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
